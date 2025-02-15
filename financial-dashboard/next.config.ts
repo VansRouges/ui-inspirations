ddd06156870e1270757af147125a4cdb4b74bd8a
freeguilders <freeguilders@gmail.com>
2025-02-15 21:30:26 +0000

    added hostname

--- financial-dashboard/next.config.ts
+++ financial-dashboard/next.config.ts
@@ -4,4 +4,15 @@ const nextConfig: NextConfig = {
   /* config options here */
 };
 
+module.exports = {
+  images: {
+      remotePatterns: [
+          {
+              protocol: 'https',
+              hostname: 'ferf1mheo22r9ira.public.blob.vercel-storage.com',
+          },
+      ],
+  },
+}
+
 export default nextConfig;
